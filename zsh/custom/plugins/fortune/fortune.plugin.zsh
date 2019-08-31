85396ab7334366e9cfaca534df0b18de37f72ad9
tingkun <tingkun@ruqiju.cn>
2019-08-31 16:34:23 +0800

    add zf for fun

--- zsh/custom/plugins/fortune/fortune.plugin.zsh
+++ zsh/custom/plugins/fortune/fortune.plugin.zsh
@@ -1,6 +1,8 @@
 
-
-COW=$(cowsay -l | tail -n +2 | tr ' ' "\n" | sort -R | head -n 1)
-COWCMD=$(echo "cowsay\ncowthink" | sort -R | head -n 1 )
-fortune | $COWCMD -f $COW | lolcat
-echo "$COW" | lolcat
+zf(){
+    COW=$(cowsay -l | tail -n +2 | tr ' ' "\n" | sort -R | head -n 1)
+    COWCMD=$(echo "cowsay\ncowthink" | sort -R | head -n 1 )
+    fortune | $COWCMD -f $COW | lolcat
+    echo "$COW" | lolcat
+}
+zf
